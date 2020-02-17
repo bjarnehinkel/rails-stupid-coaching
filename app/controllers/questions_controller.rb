0fbd5f6df26ac84a2282e8fc62e55701a355869c
Bjarne Hinkel <bjarne@hinkels.de>
2020-02-17 13:24:46 +0100

    the coach works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params['question'] == 'I am going to work'
+      @answer = 'Great!'
+    elsif /\?+/.match?(params['question'])
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    @answer
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'questions/question'
-  get 'questions/answer'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'ask', to: 'questions#question', as: :ask
+  get 'answer', to: 'questions#answer', as: :question
 end
